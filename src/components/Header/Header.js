db723ad8884a46be3bf65ee01b0c688ce981947b
Vyraal1 <kevin.afable@gmail.com>
2020-04-21 11:42:10 -0400

    feat: centered header, coloring

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,38 +5,40 @@ import styles from "./header.module.scss"
 export default () => {
   return (
     <header>
-      <h1>
-        <Link to="/">Kevin Afable</Link>
-      </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link className={styles.link} to="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link className={styles.link} to="/projects/">
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link className={styles.link} to="/blog">
-              Blog
-            </Link>
-          </li>
-          <li>
-            <Link className={styles.link} to="/resume">
-              Resume
-            </Link>
-          </li>
-          <li>
-            <Link className={styles.link} to="/about">
-              About
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.container}>
+        <h1>
+          <Link to="/">KA</Link>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link className={styles.link} to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className={styles.link} to="/projects/">
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link className={styles.link} to="/blog">
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link className={styles.link} to="/resume">
+                Resume
+              </Link>
+            </li>
+            <li>
+              <Link className={styles.link} to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
