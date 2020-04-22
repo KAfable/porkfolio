fab019f9825a09e8c9213bc094f76c7475b51978
Vyraal1 <kevin.afable@gmail.com>
2020-04-22 17:26:02 -0400

    feat: filesystem plugin config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 // Docs: https://www.gatsbyjs.org/docs/gatsby-config/
+// this is also a nodeJS file
 
 module.exports = {
   siteMetadata: {
@@ -6,5 +7,14 @@ module.exports = {
     author: "Kevin Afable",
     description: "Personal site of Kevin Afable, sometimes known as Vyraal1",
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-source-filesystem"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
 }
