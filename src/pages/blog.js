c83899dbf5334ea55fee26e94bd548a4f1c4540a
Vyraal1 <kevin.afable@gmail.com>
2020-04-27 23:13:51 -0400

    test: blog query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,35 @@
 import React from "react"
 import Layout from "../components/Layout/Layout"
 
+import { graphql, useStaticQuery } from "gatsby"
+
+// query all of the posts
+// map them through specific posts
+
 export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        nodes {
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
-      <h1>Blog Content goes here</h1>
+      {console.log(data)}
+      {data.allMarkdownRemark.nodes.map(({ frontmatter: { title, date } }) => (
+        <div>
+          <h2>{title}</h2>
+          <time>{date}</time>
+          <p>time to read: {5}</p>
+          <p>tags</p>
+        </div>
+      ))}
     </Layout>
   )
 }
