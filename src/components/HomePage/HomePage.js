b3bcec3c2127377f4f2df1e746490389c32c6278
Vyraal1 <kevin.afable@gmail.com>
2020-05-07 10:42:20 -0400

    chore: moving to laptop

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,13 +1,13 @@
-import React from "react"
-import Gears from "../Icons/Gears.jsx"
-import Loader from "../Icons/Loader.jsx"
-import styles from "./homepage.module.scss"
+import React from "react";
+import Gears from "../Icons/Gears.jsx";
+import Loader from "../Icons/Loader.jsx";
+import styles from "./homepage.module.scss";
 
 export default () => (
-  <div className={styles.banner}>
-    {/* <Gears className={styles.svgWrapper} /> */}
-    <Loader className={styles.svgWrapper} />
-    <h2>Kevin Afable</h2>
-    <h3>Software Developer</h3>
-  </div>
-)
+    <div className={styles.banner}>
+        <Gears className={styles.svgWrapper} />
+        {/* <Loader className={styles.svgWrapper} /> */}
+        <h2>Kevin Afable</h2>
+        <h3>Software Developer</h3>
+    </div>
+);
--- src/projects/picmetric/picmetric.md
+++ src/projects/picmetric/picmetric.md
@@ -1,5 +1,5 @@
 ---
-title: "Pic metric"
+title: "Pic Metric"
 date: 11/13/2019
 published: true
 tech: [Javascript, Node, Express, Knex, React, Redux, MaterialUI, Postgres, S3, RezNet]
@@ -7,3 +7,12 @@ tags: []
 template: 'project'
 previewImg: "./"
 ---
+
+## Summary
+
+
+## Technologies
+
+## My Role
+
+## What I Learned
