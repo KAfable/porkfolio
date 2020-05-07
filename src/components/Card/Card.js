2f8286510434aace8590707c95666938796e0718
Vyraal1 <kevin.afable@gmail.com>
2020-05-07 10:40:44 -0400

    feat: card scaffolding and styling

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { Link } from "gatsby";
+// import image from "../../projects/ftb-interactions/ftb-interactions-preview.png";
+import styles from "./card.module.scss";
 
-export default () => {};
+function useHover() {
+    const ref = useRef();
+    const [hovered, setHovered] = useState(false);
+
+    const enter = () => setHovered(true);
+    const leave = () => setHovered(false);
+
+    useEffect(() => {
+        const modal = ref.current;
+        modal.addEventListener("mouseenter", enter);
+        modal.addEventListener("mouseleave", leave);
+        return () => {
+            modal.removeEventListener("mouseenter", enter);
+            modal.removeEventListener("mouseleave", leave);
+        };
+    }, [ref]);
+
+    return [ref, hovered];
+}
+
+export default ({ title, url }) => {
+    const [ref, hovered] = useHover();
+
+    return (
+        <div className={styles.card} ref={ref}>
+            <h2>{title}</h2>
+            {/* <img src={image} /> */}
+            <Link to={url}>
+                <div>Preview text here</div>
+            </Link>
+            <div classname="external-links">
+                <a href="https://github.com/KAfable?tab=repositories">GH</a>
+                <a href="https://github.com/KAfable?tab=repositories">Anchor</a>
+            </div>
+        </div>
+    );
+};
 
 // has an iamge
 
