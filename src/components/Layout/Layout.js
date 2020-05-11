96a900df30a6d6c9092a4244a4f0e00639c1401b
Vyraal1 <kevin.afable@gmail.com>
2020-05-11 02:26:43 -0400

    fix: please build

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../Header/Header";
-import Footer from "../Footer.js";
+import Footer from "../Footer/Footer.js";
 
 import styles from "./layout.module.scss";
 
