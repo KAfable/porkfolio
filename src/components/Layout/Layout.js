e251a5c4c0aa83c003a0bb504fa1154d721077e2
Vyraal1 <kevin.afable@gmail.com>
2020-05-11 02:14:00 -0400

    fix: build error due to filepath resolution

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../Header/Header";
-import Footer from "../Footer";
+import Footer from "../Footer.js";
 
 import styles from "./layout.module.scss";
 
